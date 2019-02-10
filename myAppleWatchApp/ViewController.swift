62e6af3a0fbe07e870b7dd2a91fa6b32ea66210b
laibit <ghostwolf90@gmail.com>
2019-02-10 21:52:01 +0800

    Initial Commit

---
+++ myAppleWatchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myAppleWatchApp
+//
+//  Created by Laibit on 2019/2/10.
+//  Copyright © 2019 Laibit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
